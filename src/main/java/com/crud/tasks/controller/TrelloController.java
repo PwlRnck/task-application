b9c349cc98de06448d476c782c4f50ec87c9539a
“PwlRnck <p.reniecki@gmail.com>
2017-10-11 16:24:41 +0200

    tasks project: rest template

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,30 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/v1/trello")
+public class TrelloController {
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+    public void getTrelloBoards() {
+        try {
+            List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+            trelloBoards.stream().filter(trelloBoardDto -> (trelloBoardDto.getId() != null) && (trelloBoardDto.getName() != null) && (trelloBoardDto.getName().contains("Kodilla")))
+                    .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+        } catch(BoardNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+}
