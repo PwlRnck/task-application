116f3bb3ef6a6f8bb2f313c149f7c763b0778841
“PwlRnck <p.reniecki@gmail.com>
2017-10-12 14:42:11 +0200

    tasks project: nested json

---
+++ src/main/java/com/crud/tasks/domain/Badges.java
@@ -0,0 +1,14 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Badges {
+        @JsonProperty("votes")
+        private int votes;
+        @JsonProperty("attachmentsByType")
+        private AttachmentByType attachments;
+}
