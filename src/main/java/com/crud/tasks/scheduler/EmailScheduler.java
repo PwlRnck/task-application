3c44a0c4c50c503c265fb1c3b3c1b7c038f62a9e
“PwlRnck <p.reniecki@gmail.com>
2017-10-19 15:03:08 +0200

    tasks project: mail scheduler

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,43 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailScheduler {
+
+    private static final String SUBJECT = "Tasks";
+
+    @Autowired
+    private SimpleEmailService simpleEmailService;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    @Scheduled(fixedDelay = 10000)//(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        String tasksNumber;
+        switch ((int)size) {
+            case 0: tasksNumber = "no tasks";
+            break;
+            case 1: tasksNumber = "1 task";
+            break;
+            default: tasksNumber = size + " tasks";
+        }
+        simpleEmailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                null,
+                SUBJECT,
+                "Currently you have got: " + tasksNumber + " in your database."
+        ));
+    }
+}
