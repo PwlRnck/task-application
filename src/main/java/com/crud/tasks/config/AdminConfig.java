31f52bfdec30d0e8aaef071992b5e76dec5c7cef
“PwlRnck <p.reniecki@gmail.com>
2017-11-28 00:17:25 +0100

    tasks project: thymeleaf mail template

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -4,12 +4,29 @@ package com.crud.tasks.config;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
-@PropertySource("file:C:\\Users\\PR\\IdeaProjects\\tasks\\src\\main\\resources\\secret.properties")//("classpath:secret.properties")
+@PropertySources({
+        @PropertySource("file:C:\\Users\\PR\\IdeaProjects\\tasks\\src\\main\\resources\\application.properties"),
+        @PropertySource("file:C:\\Users\\PR\\IdeaProjects\\tasks\\src\\main\\resources\\secret.properties")//("classpath:secret.properties")
+})
 public class AdminConfig {
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Value("${info.company.email}")
+    private String companyEmail;
+
+    @Value("${info.company.phone}")
+    private String companyPhone;
+
 }
